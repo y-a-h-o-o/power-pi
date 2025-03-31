fa20c14006ebbcb97eb1d94920cbd7814df68678
y-a-h-o-o <wuwuwahoo@gmail.com>
2025-03-31 18:17:18 -0400

    More considerations to keep in mind :|

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ import os
 # LCD setup
 # first parameter should always be 2. Use i2cdetect -y 0 to find I2C address. last param is for backlight 
 lcd = LCD(2, 0x3F, True)
+lcd.message("POWER ON", 1)
+lcd.message("SETUP...", 2)
 
 # button setup
 wait_btn = Button(4) 
 alarm_btn = Button(17)
 bz = Buzzer(27)
-led = LED(22)
+green_led = LED(22)
+red_led = LED(18)
+
+green_led.on()
 
 # trailing average list and variable declaration
 counter = 1
@@ -31,47 +36,54 @@ def loop():
     # turn off alarm manually when this button is pressed
     if alarm_btn.is_pressed: 
         bz.off()
+        red_led.off()
    
     # gets the values from the INA
     values = get_values()     
 
-    # display to LCD
-    lcd.message("Current: " + str(values[1]), 1)
-    lcd.message("Voltage: " + str(values[0]), 2)
-   
+    # display to LCD; if freezing screen, don't change the message on LCD
+    if not wait_btn.is_pressed: 
+        lcd.message("CURRENT: " + str(values[1]), 1)
+        lcd.message("VOLTAGE: " + str(values[0]), 2)
+
     voltage = values[0]
     current = values[1]
     
     if voltage < 12.0: 
         bz.on()
+        red_led.on()
     else:
         bz.off()
+        red_led.off()
 
-    # Moves file to USB if stick is inserted
-    if not wait_btn.is_pressed:
-        usb_path = check_usb()
-        if usb_path:
-            if os.path.exists("./data.txt"):
-                lcd.message("USB INSERT", 1)
-                lcd.message("WAIT....", 2)
-                transfer_to_usb("./data.txt", usb_path) 
-            lcd.message("FILE MOVED", 1)
-            lcd.message("UNPLUG USB", 2)
+    # Moves file to USB if stick is inserted.    
+
+    usb_path = check_usb()
+    if usb_path:
+        if os.path.exists("./data.txt"):
+            lcd.message("USB INSERT", 1)
+            lcd.message("WAIT....", 2)
+            transfer_to_usb("./data.txt", usb_path) 
+        lcd.message("FILE MOVED", 1)
+        lcd.message("UNPLUG USB", 2)
+    else
+        # if 15 values are read, write the average of them to file 
+        if counter % 16 == 0:  
+            avg_voltage /= 15 
+            avg_current /= 15 
+            data = str(datetime.datetime.now()) +  " " + str(avg_voltage) + " " + str(avg_current)
+            write_to_file(data);
+            avg_voltage = 0
+            avg_current = 0
         else:
-            if counter % 16 == 0: # if 15 values are read, write the average of them to file 
-                avg_voltage /= 15 
-                avg_current /= 15 
-                data = str(datetime.datetime.now()) +  " " + str(avg_voltage) + " " + str(avg_current)
-                write_to_file(data);
-                avg_voltage = 0
-                avg_current = 0
-            else:
-                counter += 1
-                avg_voltage += voltage
-                avg_current += current
+            # increment cumulative voltage and current sum, and increment counter 
+            counter += 1
+            avg_voltage += voltage
+            avg_current += current
 
     # run loop every second; current will display every second but, every 15 readings we log the average
     # this ensures that more accurate values are stored if power fluctuates
+    
     time.sleep(1)
 
 if __name__ == "__main__":
